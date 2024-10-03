97851de41a880803db0d541c31b8185975829744
HeartyTjan <salamitijani02@gmail.com>
2024-10-03 12:24:21 +0100

    PhaseGate

---
+++ ReverseSentence.java
@@ -0,0 +1,31 @@
+public class ReverseSentence{
+
+
+	public static void main(String... args){
+
+		String userSentence = "There is tide in the affairs of men";
+
+		ReverseSentence newReverse = new ReverseSentence();
+
+		newReverse.getSentenceReverse(userSentence);
+
+	}
+	
+	
+	public void getSentenceReverse(String sentence){
+
+		String [] sen = sentence.split(" ");
+		int size = sen.length;
+
+		String [] reverse = new String [size];
+
+		for( int count = sen.length-1; count >= 0 ; count--){
+	
+				System.out.print(sen[count] + " ");
+
+			
+		}
+
+	}
+
+}
